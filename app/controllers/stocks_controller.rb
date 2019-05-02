a47d0f11ce004997aa2bc9481801e95380e2657e
Marc <akbar13@live.com>
2019-05-02 13:50:22 -0600

    Add Ajax errors and refactor stocks_controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,30 +1,26 @@
 class StocksController < ApplicationController
+
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
-      @minutes = Stock.minutes(params[:stock])
-      @average_prices = Stock.average_prices(params[:stock])
-
-      # associate stock price with time
-      @data = (0..@minutes.length-1).map do |num|
-        [@minutes[num], @average_prices[num]]
-      end
-
-      # filter data
-      @filtered_data = @data.select{ |element| element[1] >= 0}
-
-      if @stock
-        respond_to do |format|
-          # only renders the partial
-          format.js { render partial: 'users/result' }
-        end
-      else
-        flash[:danger] = "You have entered an invalid ticker symbol"
-        redirect_to my_portfolio_path
-      end
+    if params[:stock].blank?
+      flash.now[:danger] = "You have entered an empty search string"
     else
-      flash[:danger] = "You have entered an empty search string"
-      redirect_to my_portfolio_path
+      @stock = Stock.new_from_lookup(params[:stock])
+      if @stock
+        @minutes = Stock.minutes(params[:stock])
+        @average_prices = Stock.average_prices(params[:stock])
+        # associate stock price with time and put in chart format
+        @data = (0..@minutes.length-1).map do |num|
+          [@minutes[num], @average_prices[num]]
+        end
+        # filter data
+        @filtered_data = @data.select{ |element| element[1] >= 0}
+      else
+        flash.now[:danger] = "You have entered an invalid ticker symbol"
+      end
+    end
+    respond_to do |format|
+      format.js { render partial: 'users/result' }
     end
   end
+
 end
