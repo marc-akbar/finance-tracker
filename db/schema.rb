72fdb5ae72a3942bbdf44055b801a40a5a160645
Marc <akbar13@live.com>
2019-06-06 17:24:06 -0600

    Add UserStocks table and save function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_042718) do
+ActiveRecord::Schema.define(version: 2019_06_06_213818) do
 
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2019_05_02_042718) do
     t.datetime "updated_at", null: false
     t.decimal "low"
     t.decimal "high"
+  end
+
+  create_table "user_stocks", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "stock_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["stock_id"], name: "index_user_stocks_on_stock_id"
+    t.index ["user_id"], name: "index_user_stocks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
