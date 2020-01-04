5307f3859d0319c414aba8562689c010f1587fbe
marc <marc.akbar@gmail.com>
2020-01-04 14:04:27 -0700

    Clean up chart and move arguments to global file

---
+++ config/initializers/chartkick.rb
@@ -0,0 +1,19 @@
+Chartkick.options = {
+  height: "500px",
+  colors: ["#1438ff", "#f03232"],
+  prefix: "$",
+  discrete: true,
+  xtitle: "Eastern Standard Time",
+  points: false,
+  thousands: ",",
+  library: {
+    animation: {easing: 'easeOutQuad'},
+    scales: {
+      yAxes: [{
+        ticks: {
+          display: false
+        }
+        }]
+    }
+  }
+}
